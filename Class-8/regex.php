64fd8417e6bb94f4e3e23f04b23c24c7a8fb5d5d
Mamun Abdullah <mamunabdullah.codes@gmail.com>
2024-01-06 02:14:42 +0600

    Class 8, regex-preg_match of mobile, password and email validation

--- Class-8/regex.php
+++ Class-8/regex.php
@@ -17,3 +17,23 @@ $str = "Hello World. Hello Bangladesh";
 $pattern = "/hello/i";
 $replace = "Hi";
 echo preg_replace($pattern, $replace, $str);
+echo "<br>";
+
+// regex pattern matching (regular expression)
+// [] - character classes
+$mobile = "01712345678";
+$pattern = "/^01[0-9]{9}$/";
+echo preg_match($pattern, $mobile);
+echo "<br>";
+
+// regex strong password with spacial characters
+$password = "Abcd@12300";
+// $pattern = "/^[a-zA-Z0-9@#%.]{8,}$/"; // matches only these characters if found then return true
+$pattern = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/"; // use , at {} to use at least method
+echo preg_match($pattern, $password);
+echo "<br>";
+
+// regex email validation
+$email = "kuddus@boyati.com";
+$pattern = "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z.]{2,5}$/";
+echo preg_match($pattern, $email);
